410bcdc9767e347acd20b854c09e2fb8817badb1
Luca Mariani <lmar76@gmail.com>
2023-05-08 19:46:24 +0200

    Update mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,2 +1,3 @@
-vim.cmd([[nnoremap <C-t> :Neotree toggle reveal<cr>]])
-vim.cmd([[nnoremap <C-a> :AerialToggle!<cr>]])
+vim.keymap.set('n', '<C-a>', ':AerialToggle!<cr>')
+vim.keymap.set('n', '<C-t>', ':Neotree toggle reveal<cr>')
+vim.keymap.set('n', '<C-w>', ':set wrap!<cr>')
