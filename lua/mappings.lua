d4d105a2e8ba11f2faa1d0dc3d2da864d8e8bebb
Luca Mariani <lmar76@gmail.com>
2023-05-08 20:37:03 +0200

    Add aerial and neo-tree config files

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,3 +1,1 @@
-vim.keymap.set('n', '<C-a>', ':AerialToggle!<cr>')
-vim.keymap.set('n', '<C-t>', ':Neotree toggle reveal<cr>')
-vim.keymap.set('n', '<C-w>', ':set wrap!<cr>')
+vim.keymap.set('n', '<M-w>', ':set wrap!<cr>')
---
+++ lua/plugins_config/neo-tree.lua
@@ -0,0 +1,1 @@
+vim.keymap.set('n', '<C-t>', ':Neotree toggle reveal<cr>')
